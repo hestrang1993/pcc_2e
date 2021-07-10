6ccc1bcb9841c677a520d7fbe4bd6e56b24a73d3
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-07-10 14:02:07 -0400

    feat(Settings): add bullet properties
    Add properties for the player's bullets.
    Namely:
    1. The bullet's speed (pixels per cycle)
    2. The bullet's width (pixels)
    3. The bullet's height (pixels)
    4. The bullet's color (RGB)
    
    Harrison Strang

--- chapter_12/my_game/settings.py
+++ chapter_12/my_game/settings.py
@@ -11,9 +11,22 @@ class Settings:
         self._screen_width = 1200
         self._screen_height = 800
         self._screen_dimensions = (self.screen_width, self.screen_height)
-        self._channel_value = 0
-        self._background_color = (self.channel_value, self.channel_value, self.channel_value)
+        self._background_color_channel_value = 0
+        self._background_color = (
+                self.background_color_channel_value,
+                self.background_color_channel_value,
+                self.background_color_channel_value
+        )
         self._ship_speed = 1.5
+        self._bullet_speed = 1.0
+        self._bullet_width = 4
+        self._bullet_height = 16
+        self._bullet_color_channel_value = 255
+        self._bullet_color = (
+                self.bullet_color_channel_value,
+                self._bullet_color_channel_value,
+                self.bullet_color_channel_value
+        )
 
     @property
     def screen_width(self):
@@ -39,9 +52,9 @@ class Settings:
         return self._screen_dimensions
 
     @property
-    def channel_value(self):
+    def background_color_channel_value(self):
         """int: The value for each RGB channel."""
-        return self._channel_value
+        return self._background_color_channel_value
 
     @property
     def background_color(self):
@@ -52,3 +65,28 @@ class Settings:
     def ship_speed(self):
         """float: The horizontal speed of the player."""
         return self._ship_speed
+
+    @property
+    def bullet_speed(self):
+        """float: The vertical speed of the player's bullets, in pixels per cycle."""
+        return self._bullet_speed
+
+    @property
+    def bullet_width(self):
+        """int: The pixel width of the player's bullets."""
+        return self._screen_width
+
+    @property
+    def bullet_height(self):
+        """int: The pixel height of the player's bullets."""
+        return self._bullet_height
+
+    @property
+    def bullet_color_channel_value(self):
+        """int: The value for all RGB channels for the player's bullets."""
+        return self._bullet_color_channel_value
+
+    @property
+    def bullet_color(self):
+        """tuple[int, int, int]: The RGB coloring for the player's bullets."""
+        return self._bullet_color
