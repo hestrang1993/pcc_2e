6ed756533d9c44ea7863ec8fd90d3a749d266cfb
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-07-15 20:22:09 -0400

    refactor(my_game): move my game files to another directory
    This is so I can look at file across multiple chapter directories easily
    .
    Also, I want to see if I can do something like __init__.py
    Remove old game directory.
    
    Harrison Strang

---
+++ my_game/my_bullets.py
@@ -0,0 +1,79 @@
+﻿# coding=utf-8
+"""
+The :module:`my_bullets` module will contain the class that handles the player's bullets.
+"""
+import pygame
+from pygame.sprite import Sprite
+
+
+class Bullet(Sprite):
+    """
+    The :class:`Bullet` class extends the :class:`Sprite` class from :module:`pygame.sprite`.
+    This class will handle how the player's bullets will behave.
+    """
+
+    def __init__(self, alien_invasion):
+        """
+        Create a new :class:`Ship` instance.
+
+        Parameters
+        ----------
+        alien_invasion : chapter_12.my_game.my_alien_invasion.AlienInvasion
+            The alien invasion game I want to add the ship to.
+        """
+        super().__init__()
+        self.screen = alien_invasion.screen
+        self.settings = alien_invasion.settings
+        self.bullet_color = self.settings.bullet_color
+        self.bullet_rectangle = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        """
+        pygame.Rect: The rectangle that defines the bullet's hit box.
+        """
+        self.bullet_rectangle.midtop = alien_invasion.ship.ship_rectangle.midtop
+        self._bullet_y = float(self.bullet_rectangle.y)
+
+    @property
+    def bullet_y(self):
+        """float: The y-coordinate of the player's bullet."""
+        return self._bullet_y
+
+    @bullet_y.setter
+    def bullet_y(self, new_bullet_height):
+        self._bullet_y = new_bullet_height
+
+    def _get_bullet_rectangle_at_origin(self):
+        """
+        Create and get a bullet that will spawn at the origin (0, 0) of the screen.
+
+        The bullet's x-coordinate will be adjusted soon after.
+
+        Returns
+        -------
+        pygame.Rect
+            The rectangle that will define the player's bullet.
+        """
+        width = self.settings.bullet_width
+        height = self.settings.bullet_height
+        bullet_rectangle = pygame.Rect(0, 0, width, height)
+        return bullet_rectangle
+
+    def update(self):
+        """
+        Update the vertical position of the player's bullet.
+
+        Returns
+        -------
+        None
+        """
+        self.bullet_y -= self.settings.bullet_speed
+        self.bullet_rectangle.y = self.bullet_y
+
+    def draw_bullet(self):
+        """
+        Draw the bullet on the screen.
+
+        Returns
+        -------
+        None
+        """
+        pygame.draw.rect(self.screen, self.bullet_color, self.bullet_rectangle)
---
+++ my_game/settings.py
@@ -0,0 +1,95 @@
+﻿# coding=utf-8
+"""
+The :module:`my_settings` module contains many of the settings I will need to play the game.
+"""
+
+
+class Settings:
+    """The :class:`Settings` stores all the settings for my version of Alien Invasion."""
+
+    def __init__(self):
+        self._screen_width = 1200
+        self._screen_height = 800
+        self._screen_dimensions = (self.screen_width, self.screen_height)
+        self._background_color_channel_value = 0
+        self._background_color = (
+                self.background_color_channel_value,
+                self.background_color_channel_value,
+                self.background_color_channel_value
+        )
+        self._ship_speed = 1.5
+        self._bullet_speed = 1.0
+        self._bullet_width = 8
+        self._bullet_height = 32
+        self._bullet_color_channel_value = 250
+        self._bullet_color = (self.bullet_color_channel_value, self.bullet_color_channel_value,
+                              self.bullet_color_channel_value)
+        self._number_of_bullets_allowed = 3
+
+    @property
+    def screen_width(self):
+        """int: The pixel width of the screen."""
+        return self._screen_width
+
+    @screen_width.setter
+    def screen_width(self, width):
+        self._screen_width = width
+
+    @property
+    def screen_height(self):
+        """int: The pixel height of the screen."""
+        return self._screen_height
+
+    @screen_height.setter
+    def screen_height(self, height):
+        self._screen_height = height
+
+    @property
+    def screen_dimensions(self):
+        """tuple[int, int]: The pixel width and height of the screen, respectively."""
+        return self._screen_dimensions
+
+    @property
+    def background_color_channel_value(self):
+        """int: The value for each RGB channel."""
+        return self._background_color_channel_value
+
+    @property
+    def background_color(self):
+        """tuple[int, int, int]: The background color for the game window."""
+        return self._background_color
+
+    @property
+    def ship_speed(self):
+        """float: The horizontal speed of the player."""
+        return self._ship_speed
+
+    @property
+    def bullet_speed(self):
+        """float: The vertical speed of the player's bullets, in pixels per cycle."""
+        return self._bullet_speed
+
+    @property
+    def bullet_width(self):
+        """int: The pixel width of the player's bullets."""
+        return self._bullet_width
+
+    @property
+    def bullet_height(self):
+        """int: The pixel height of the player's bullets."""
+        return self._bullet_height
+
+    @property
+    def bullet_color_channel_value(self):
+        """int: The value for all RGB channels for the player's bullets."""
+        return self._bullet_color_channel_value
+
+    @property
+    def bullet_color(self):
+        """tuple[int, int, int]: The RGB coloring for the player's bullets."""
+        return self._bullet_color
+
+    @property
+    def number_of_bullets_allowed(self):
+        """int: The total number of bullets allowed onscreen."""
+        return self._number_of_bullets_allowed
