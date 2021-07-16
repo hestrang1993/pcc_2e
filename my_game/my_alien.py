2f7ea29d930d55dd0bf63f7740582dd81a614899
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-07-15 23:33:23 -0400

    feat(alien.py): create an alien NPC on screen

--- my_game/my_alien.py
+++ my_game/my_alien.py
@@ -4,10 +4,14 @@ The :module:`alien` will handle the behaviors and characteristics of the alien N
 
 This will be done through the :class:`Alien`, which extends :class:`pygame.sprite.Sprite`.
 """
+import pygame
 from pygame.sprite import Sprite
 
 
 class Alien(Sprite):
+    """
+    The :class:`Alien` will handle the behaviors and characteristics of the alien NPCs.
+    """
 
     def __init__(self, alien_invasion):
         """
@@ -20,4 +24,29 @@ class Alien(Sprite):
         """
         super().__init__()
         self.screen = alien_invasion.screen
-        self.alien_image = None
+        self._alien_image_file_path = 'images/alien_resized.png'
+        self._alien_image = pygame.image.load(self.alien_image_file_path)
+        self.alien_rectangle = self.alien_image.get_rect()
+        """
+        pygame.Rect: The hit box for the alien NPC sprites.
+        """
+        self.alien_rectangle.x = self.alien_rectangle.width
+        self.alien_rectangle.y = self.alien_rectangle.height
+        self.alien_x = float(self.alien_rectangle.x)
+        """
+        float: The x-position of an aline NPC sprite.
+        """
+
+        # Needed to load in the images correctly.
+        self.image = self.alien_image
+        self.rect = self.alien_rectangle
+
+    @property
+    def alien_image_file_path(self):
+        """str: The file path to the image for the alien NPCs."""
+        return self._alien_image_file_path
+
+    @property
+    def alien_image(self):
+        """pygame.Surface: The image to use for the alien NPC sprites."""
+        return self._alien_image
