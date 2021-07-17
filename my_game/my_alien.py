161132f18ee2870a4f3c29ca09e547c9224f22d8
hestrang1993 <48037715+hestrang1993@users.noreply.github.com>
2021-07-16 21:05:36 -0400

    feat(Alien): check fleet settings and screen edge

--- my_game/my_alien.py
+++ my_game/my_alien.py
@@ -19,11 +19,12 @@ class Alien(Sprite):
 
         Parameters
         ----------
-        alien_invasion: chapter_12.my_game.my_alien_invasion.AlienInvasion
+        alien_invasion: my_game.my_alien_invasion.AlienInvasion
             The alien invasion game I want to add the ship to.
         """
         super().__init__()
         self.screen = alien_invasion.screen
+        self._settings = alien_invasion.settings
         self._alien_image_file_path = 'images/alien_resized.png'
         self._alien_image = pygame.image.load(self.alien_image_file_path)
         self.alien_rectangle = self.alien_image.get_rect()
@@ -42,6 +43,11 @@ class Alien(Sprite):
         self.rect = self.alien_rectangle
 
     @property
+    def settings(self):
+        """my_game.settings.Settings: The settings for the alien NPC sprite."""
+        return self._settings
+
+    @property
     def alien_image_file_path(self):
         """str: The file path to the image for the alien NPCs."""
         return self._alien_image_file_path
@@ -50,3 +56,27 @@ class Alien(Sprite):
     def alien_image(self):
         """pygame.Surface: The image to use for the alien NPC sprites."""
         return self._alien_image
+
+    def check_edges(self):
+        """
+        Return ``True`` if the alien sprite hits the edge of the screen.
+
+        Returns
+        -------
+        bool
+            True if the alien hits the edge of the screen.
+        """
+        screen_rectangle = self.screen.get_rect()
+        if self.rect.right >= screen_rectangle.right or self.rect.left <= 0:
+            return True
+
+    def update(self):
+        """
+        Move the alien to the right.
+
+        Returns
+        -------
+        None
+        """
+        self.alien_x += self.settings.alien_speed * self.settings.fleet_horizontal_direction
+        self.alien_rectangle.x = self.alien_x
